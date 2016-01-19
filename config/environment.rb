0fbf38267a82b879d5416842ae3649d3bc1e54b4
Jeremy <sensoph@gmail.com>
2016-01-19 15:16:00 -0600

    fix carrierwave

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+
 # Initialize the Rails application.
 Rails.application.initialize!
+
+require 'carrierwave'
+require 'carrierwave/orm/activerecord'
